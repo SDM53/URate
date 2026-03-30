59e8823e0822a27ccf77b7554a29c3acf4d7aac8
SMorfin <sdmorfin53@gmail.com>
2026-03-29 19:42:05 -0500

    Cleanup1

--- src/router.js
+++ src/router.js
@@ -1,15 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from './views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from './views/HomeView.vue';
 
 const routes = [
   { path: '/', component: HomeView },
   // These two are placeholders so RouterLink doesn't throw errors
-  // We'll build them out properly later
   { path: '/post/:id', component: { template: '<div class="p-8 text-gray-500">Post page coming soon</div>' } },
-  { path: '/create',   component: { template: '<div class="p-8 text-gray-500">Create page coming soon</div>' } },
-]
+  { path: '/create', component: { template: '<div class="p-8 text-gray-500">Create page coming soon</div>' } },
+];
 
 export default createRouter({
   history: createWebHistory(),
   routes,
-})
+});
